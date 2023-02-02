73e613fc6161bb5983316fdc7aae127052a479c5
sanszajn96 <47297063+sanszajn96@users.noreply.github.com>
2023-02-02 14:55:42 +0100

    Add files via upload

---
+++ src/components/TableRow.jsx
@@ -0,0 +1,22 @@
+import TableCell from './TableCell'
+const TableRow = ({flight}) => {
+
+  const words = Object.values(flight)
+
+  console.log(words)
+
+    return (
+
+      <tr>
+        <td>✈</td>
+        {words?.map((word, index) => (
+        <TableCell key={index} word={word} index={index} />
+        ))}
+  
+      </tr>
+      
+  
+    )
+  }
+  
+  export default TableRow
